5468ccea9931eb82f55146426b9319965b1975ab
sam <samuelabraha@gmail.com>
2026-05-14 17:35:12 +0200

    fix: error middleware must be last

--- server/src/index.js
+++ server/src/index.js
@@ -37,10 +37,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use(errorMiddleware);
 
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
+app.use(errorMiddleware);
 
 async function testConnection() {
   try {
